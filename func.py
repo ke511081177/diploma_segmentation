8e76a38235d1877dae856dd71bdebf21686c5c2f
hank-influx <86759251+hank-influx@users.noreply.github.com>
2021-07-22 16:43:30 +0800

    Add files via upload

---
+++ func.py
@@ -0,0 +1,35 @@
+
+import numpy as np
+import cv2
+
+
+def angle_cos(p0, p1, p2):
+    d1, d2 = (p0-p1).astype('float'), (p2-p1).astype('float')
+    return abs( np.dot(d1, d2) / np.sqrt( np.dot(d1, d1)*np.dot(d2, d2) ) )
+
+def find_squares(img):
+    img = cv2.GaussianBlur(img, (5, 5), 0)
+    squares = []
+    for gray in cv2.split(img):
+        for thrs in range(0, 255, 26):
+            if thrs == 0:
+                bin = cv2.Canny(gray, 0, 50, apertureSize=5)
+                bin = cv2.dilate(bin, None)
+            else:
+                _retval, bin = cv2.threshold(gray, thrs, 255, cv2.THRESH_BINARY)
+            bin, contours, _hierarchy = cv2.findContours(bin, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
+            for cnt in contours:
+                cnt_len = cv2.arcLength(cnt, True)
+                cnt = cv2.approxPolyDP(cnt, 0.02*cnt_len, True)
+                #cv2.contourArea(cnt) > 1000 and 
+                #and cv2.isContourConvex(cnt)
+                if len(cnt) == 4 :
+                    cnt = cnt.reshape(-1, 2)
+                    max_cos = np.max([angle_cos( cnt[i], cnt[(i+1) % 4], cnt[(i+2) % 4] ) for i in range(4)])
+                    #print(cnt)
+                    a = (cnt[1][1] - cnt[0][1])
+
+                    #if max_cos < 0.1 and a < img.shape[0]*0.8:
+
+                    squares.append(cnt)
+                    return squares
