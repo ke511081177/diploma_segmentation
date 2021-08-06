6cc5904a8abffc3a42c2db1d4c23927a1afd1e27
ke511081177 <ke511081177@gmail.com>
2021-08-06 14:03:08 +0800

    Update schoolClassification.py

--- schoolClassification.py
+++ schoolClassification.py
@@ -13,7 +13,7 @@ import tensorflow as tf
 physical_devices = tf.config.experimental.list_physical_devices('GPU')
 tf.config.experimental.set_memory_growth(physical_devices[0], True)
 
-os.environ["GOOGLE_APPLICATION_CREDENTIALS"]="subtle-fulcrum-319206-415ab8f59c71.json"
+os.environ["GOOGLE_APPLICATION_CREDENTIALS"]="XXXXX.json"
 
 from google.cloud import vision
 
